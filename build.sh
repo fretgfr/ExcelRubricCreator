80fd071cfd644b8f4190681b2c84af9edbd2b0db
Robert Drescher <robert@macombhometeam.com>
2021-12-16 00:28:49 -0500

    Update build files

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@ mkdir build 2>/dev/null
 
 cp ExcelGradingRubricCreator.py build/
 cd build
-pyinstaller --nowindowed --onefile ExcelGradingRubricCreator.py
+pyinstaller --windowed --onefile ExcelGradingRubricCreator.py
